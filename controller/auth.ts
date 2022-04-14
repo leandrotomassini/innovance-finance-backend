8ab7516abd367be30220830a210a83973d9565be
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-04-14 17:12:34 -0300

    Leer el email

--- controller/auth.ts
+++ controller/auth.ts
@@ -1,14 +1,17 @@
 import { Request, Response } from 'express';
+import { QueryResult } from 'pg';
 
+import { pool } from '../database/connection';
 
-export const googleSignIn = async (req: Request, res: Response) => {
+export const googleSignIn = async (req: Request, res: Response): Promise<QueryResult> => {
 
     const { google_email, name, img } = req.body;
 
     try {
 
         // TODO: verificar  el email
-
+        const response: QueryResult = await pool.query('SELECT * FROM public.usuario WHERE email = $1', [google_email]);
+        const respuesta = response.rows[0].email;
         // Verificiar si es un email autorizado y el estado del usuario en la DB
 
         // Generar un JWT
@@ -16,9 +19,9 @@ export const googleSignIn = async (req: Request, res: Response) => {
         res.json({
             ok: true,
             msg: 'Inicio de sesión con Google correcto',
-            google_email,
             name,
-            img
+            img,
+            respuesta
         })
 
     } catch (error) {

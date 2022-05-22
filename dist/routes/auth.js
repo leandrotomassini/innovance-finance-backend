c0cea198f76b5c087d0ff24c6a1737b72bd215a2
Leandro C. Tomassini <leandroctomassini@gmail.com>
2022-05-22 14:59:35 -0300

    Version 2 blank

--- dist/routes/auth.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const auth_1 = require("../controller/auth");
-const router = (0, express_1.Router)();
-router.post('/', auth_1.login);
-exports.default = router;
-//# sourceMappingURL=auth.js.map
